2b01707f27378b53a14135777fe3b3fde04a822a
Paola Vallejo <pvallej3@eafit.edu.co>
2020-03-02 18:47:05 -0500

    Proyecto creado con DJango

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\pvallej3.cinfo\documents\cursoseafit\pi1\20201\example\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
